94614b843dc2b839afb97763474f69a852e1622e
Anggoro Yudho N. <70189451+anggoro-yn@users.noreply.github.com>
2024-12-11 11:57:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ st.title('Visualisasi Kelulusan dan Progress Peserta')
 st.header('Selamat kepada peserta berikut yang telah menyelesaikan seluruh course')
 
 # Filter peserta yang telah menyelesaikan 8 course
-completed_all_courses = data[data['Total Course yang Sudah Diselesaikan'] == 8]['Name'].tolist()
+completed_all_courses = data[data['Progress Belajar Percentage'] == '100%']['Name'].tolist()
 
 # Display the names of participants who completed all courses
 jumlah = len(completed_all_courses)
